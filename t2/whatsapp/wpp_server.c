a898d4480aa7763c2eb1d43af64a38839c0a74ce
fsapipez <felipe_angnes@hotmail.com>
2017-06-29 21:14:05 -0300

    whatsapp incomplete

--- t2/whatsapp/wpp_server.c
+++ t2/whatsapp/wpp_server.c
@@ -485,7 +485,7 @@ void groupMembers(char *groupData)
 		//ctt = (struct stConnectedContacts*)malloc(sizeof(struct stConnectedContacts));
 		//memset(ctt, 0, sizeof(struct stConnectedContacts));
 		printf("groupData[i]: %c\n", groupData[i]);
-		if(groupData[i] != ' ' && groupData[i] != NULL)
+		if(groupData[i] != ' ' && groupData[i] != '\0')
 		{
 			name[i] = groupData[i];
 			printf("name[i]: %c\n", name[i]);
